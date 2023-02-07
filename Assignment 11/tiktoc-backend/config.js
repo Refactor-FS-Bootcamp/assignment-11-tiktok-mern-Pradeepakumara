99394c02634c6a86b5bfaccd25782bd41ba1f474
Pradeepakumara K R <pradeepkumarkaggundi@gmail.com>
2023-02-06 14:39:47 -1200

    TikTok MERN Clone

--- Assignment 11/tiktoc-backend/config.js
+++ Assignment 11/tiktoc-backend/config.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 
-const url_connection = "mongodb+srv://<username>:<password>@cluster0.flks18f.mongodb.net/tik-tok?retryWrites=true&w=majority";
-// I removed my username and password because it is my local database
-// please copy and paste data to database 
-// please read text file, it has all data
+const url_connection = "mongodb+srv://pradeep_dev:pradeep_dev@cluster0.flks18f.mongodb.net/tik-tok?retryWrites=true&w=majority";
 
 mongoose.set('strictQuery', true);
 
